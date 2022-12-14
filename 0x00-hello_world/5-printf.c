8e3fcf9d04072409ff95a74cf424c5b90852b477
YemiAl <oluyemialabi@gmail.com>
2022-12-14 13:45:30 -0800

    Added script 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
  * Main - A program that prints a line with printf
  *
  * Return: 0
-*/
+ */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n"); /*prints the quoted statement */
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
 
