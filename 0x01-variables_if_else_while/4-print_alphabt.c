9a3d1a8939a6d7981f3345457d3874f3a893e9aa
YemiAl <oluyemialabi@gmail.com>
2022-12-16 07:22:33 -0800

    Added script 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all alphabets in lowercase except q and e
+ *
+ * Return: (alphabets)
+ */
+
+int main(void)
+
+{
+
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+if ((i == 'e') || (i == 'q'))
+
+{
+continue;
+}
+
+putchar(i);
+}
+
+putchar('\n');
+}
