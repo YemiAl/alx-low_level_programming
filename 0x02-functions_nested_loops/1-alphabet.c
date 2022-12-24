12c1897e8111882242f9db343affa27f91dd6001
YemiAl <oluyemialabi@gmail.com>
2022-12-23 23:18:11 -0800

    Removed main from script 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,20 @@
 #include "main.h"
 
 /**
- * main - Program that prints the lowercase alphabets
+ * print_alphabet - function that prints the lowercase alphabets
  *
  * Return: 0
  */
 
-int main(void)
+
+void print_alphabet(void)
 {
-	void print_alphabet(void)
-	{
-		char i;
 
-		for (i = 'a'; i <= 'z'; i++)
-			_putchar(i);
+	char i;
 
-		_putchar('\n');
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+
+	_putchar('\n');
 	}
-
-
-	print_alphabet();
-
-
 }
