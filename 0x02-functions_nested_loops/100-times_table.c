d060b3a27227731113f4f26b989106d86a6932b3
YemiAl <oluyemialabi@gmail.com>
2022-12-30 19:10:03 -0800

    Added script 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_times_table - program that prints n times table
+ * @n: number of times table to be printed
+ */
+
+void print_times_table(int n)
+{
+	int j = 0;
+	int a, b;
+
+	if ((n <= 15) && (n >= 0))
+	{
+		for (a = 0; a <= n; a++) /*print 10 lines of increasing diff */
+		{
+			int i = 0;
+
+			_putchar('0'); /* print the first 0 */
+			for (b = 0; b <= (n - 1); b++)
+			{
+				i = i + j;
+				if (i > 9 && i < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((i / 10) + '0');
+					_putchar((i % 10) + '0');
+				}
+				else if (i > 99 && i < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((i / 100) + '0');
+					_putchar(((i / 10) % 10) + '0');
+					_putchar((i % 10) + '0');
+				}
+				else
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(i + '0');
+			}
+			j++;
+			_putchar('\n');
+		}
+	}
+}
