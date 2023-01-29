9fb66c8bba7a2d5ad040a2716e3f48cc05e3d978
YemiAl <oluyemialabi@gmail.com>
2023-01-29 13:49:34 -0800

    Added script 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * main - program that adds two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: sum
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1) /* if no number is passed */
+	{
+		printf("0\n");
+	}
+
+	else
+	{
+		int sum = 0;
+
+		int str_to_len;
+
+		int i;
+
+		for (i = 1; i < argc; i++)
+		{
+			if (!isdigit(*argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+			else
+			{
+				str_to_len = atoi(argv[i]);
+
+				sum = sum + str_to_len;
+			}
+		}
+
+		printf("%i\n", sum);
+	}
+
+	return (0);
+}
