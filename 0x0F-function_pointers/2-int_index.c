0e28ae174330eada45833f6599046ec374697963
YemiAl <oluyemialabi@gmail.com>
2023-02-18 10:17:38 -0800

    Edited script 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -20,11 +20,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		int i;
 
-		for (i = 0; i < size; i++)
+		if (array != NULL && cmp != NULL)
 		{
-			if (cmp(array[i]))
+
+			for (i = 0; i < size; i++)
 			{
-				return (i);
+				if (cmp(array[i]))
+				{
+					return (i);
+				}
 			}
 		}
 
