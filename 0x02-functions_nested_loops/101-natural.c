c2a99a8d5353d24433f5af558f10bc4f2b527787
YemiAl <oluyemialabi@gmail.com>
2023-03-21 21:09:58 -0700

    Added script 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - program that sums the multiples of 3, 5 below 1024
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (((i % 3) == 0) || ((i % 5) == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+}
