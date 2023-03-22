63ab45ab6eb5588250c40a70d484d6b84ce05d25
YemiAl <oluyemialabi@gmail.com>
2023-03-21 21:23:53 -0700

    Added script 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,1 +1,30 @@
 #include <stdio.h>
+
+int main(void)
+{
+    int i = 3;
+
+    unsigned int x = 1, y = 2, sum;
+
+    printf("%u, %u, ", x, y);
+
+    while (i <= 50)
+    {
+        sum = x + y;
+
+        printf("%u", sum);
+
+        if (i != 50)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+
+        x = y;
+
+        y = sum;
+
+        i++;
+    }
+    putchar('\n');
+}
