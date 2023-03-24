9302c17eec53750b372c7aaa56f4b2cc7c825a3d
YemiAl <oluyemialabi@gmail.com>
2023-03-23 17:28:34 -0700

    Edited scripts 0 and 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,11 +9,11 @@
 
 int _isupper(int c)
 {
-    if (c >= 65 && c <= 90)
-    {
-        return (1);
-    }
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
 
-    else
-        return (0);
+	else
+		return (0);
 }
