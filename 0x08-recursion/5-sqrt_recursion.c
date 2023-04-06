30db4022b19c20e6fa082e95b77bdc7ebc2c50f8
YemiAl <oluyemialabi@gmail.com>
2023-04-05 21:57:13 -0700

    Added script 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find sqrt of
+ * Return: natural square root or -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	int count = 0;
+	if ((n % 2) == 1)
+	{
+		return (-1);
+	}
+
+	if (n >= 2)
+	{
+		count++;
+		count = count + _sqrt_recursion(n / 2);
+		return (count);
+	}
+
+	else
+	{
+		return (0);
+	}
