f059de158bff17f992fd7735b16ff39be6418fbd
YemiAl <oluyemialabi@gmail.com>
2023-06-14 03:36:03 +0100

    Added script 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - program that points to a 2 dimensional array
+ * @width: width of the array
+ * @height: height of the array
+ *
+ * Return: a double pointer to the array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **dptr;
+
+	if ((height <= 0) || (width <= 0))
+	{
+		return (0);
+	}
+	dptr = malloc(height * sizeof(int *));
+
+	if (dptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			dptr[i] = malloc(width * sizeof(int));
+			if (dptr[i] == NULL)
+			{
+				while (i > 0)
+				{
+					free(dptr[i - 1]);
+					i--;
+				}
+
+				free(dptr);
+				return (NULL);
+			}
+			else
+			{
+				for (j = 0; j < width; j++)
+				{
+					dptr[i][j] = 0;
+				}
+			}
+		}
+		return (dptr);
+	}
+}
