a96e1dfcabb1089a97537c1c55a6009b65de6384
YemiAl <oluyemialabi@gmail.com>
2023-06-21 20:43:46 +0100

    Added script 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function pointer that also takes an array as an argument
+ * @array: array to be actioned on
+ * @size: size of the array
+ * @action: function pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
